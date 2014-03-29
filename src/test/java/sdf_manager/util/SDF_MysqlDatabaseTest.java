fdf76d656444260fd07c6dfdcea457036af6c6b9
Søren Roug <soren.roug@eea.europa.eu>
2014-03-29 11:04:22 +0000

    An inactive unit test to see if insert_birds_new.sql is being run. Refs #16607

--- src/test/java/sdf_manager/util/SDF_MysqlDatabaseTest.java
+++ src/test/java/sdf_manager/util/SDF_MysqlDatabaseTest.java
@@ -59,17 +59,16 @@ public class SDF_MysqlDatabaseTest {
         ResultSet rs;
         int num;
 
+        st = con.createStatement();
         // Check how many species are in the database
         // 2516 from insert_ref_species.sql and 13 from Update_RefSpecies_version3.sql
-        st = con.createStatement();
         rs = st.executeQuery("SELECT COUNT(*) FROM ref_species");
         rs.next();
         num = rs.getInt(1);
-        assertEquals(2529, num);
+        assertEquals(2516 + 13, num);
 
         // Check how many NUTS codes are loaded
         // 299 from insert_ref_nuts.sql
-        //st = con.createStatement();
         rs = st.executeQuery("SELECT COUNT(*) FROM ref_nuts");
         rs.next();
         num = rs.getInt(1);
@@ -77,17 +76,45 @@ public class SDF_MysqlDatabaseTest {
 
         // Check how many habitats are loaded
         // 231 from insert_ref_habitats.sql and 2 from Update_RefHabitats_version3.sql
-        //st = con.createStatement();
         rs = st.executeQuery("SELECT COUNT(*) FROM ref_habitats");
         rs.next();
         num = rs.getInt(1);
-        assertEquals(233, num);
+        assertEquals(231 + 2, num);
+
+        // Check how many data quality codes are loaded
+        // 4 from insert_ref_data_quality.sql and 1 from insert_RefDataQual_version3.sql
+        rs = st.executeQuery("SELECT COUNT(*) FROM ref_data_quality");
+        rs.next();
+        num = rs.getInt(1);
+        assertEquals(4 + 1, num);
+
+        // Check that Alter_Ref_Birds_table.sql and insert_birds.sql are run
+        // 78 from insert_birds.sql have an alternative scientific name
+        rs = st.executeQuery("SELECT COUNT(*) FROM ref_birds WHERE REF_BIRDS_ALT_SCIENTIFIC_NAME != ''");
+        rs.next();
+        num = rs.getInt(1);
+        assertEquals(78, num);
+
+        // Check that the REF_BIRDS_CODE_NEW is up to date - i.e. insert_birds_new.sql has been run
+        // birds A527 and A567 are being changed to 1 by insert_birds_new.sql
+        rs = st.executeQuery("SELECT COUNT(*) FROM ref_birds WHERE REF_BIRDS_CODE_NEW='1' AND REF_BIRDS_CODE IN ('A527','A567')");
+        rs.next();
+        num = rs.getInt(1);
+        //assertEquals(2, num);
+        
+        // Check that the REF_BIRDS_CODE_NEW is up to date - i.e. insert_birds_new.sql has been run
+        // birds A261 and A276 are being changed to 0 by insert_birds_new.sql
+        rs = st.executeQuery("SELECT COUNT(*) FROM ref_birds WHERE REF_BIRDS_CODE_NEW='0' AND REF_BIRDS_CODE IN ('A261','A276')");
+        rs.next();
+        num = rs.getInt(1);
+        //assertEquals(2, num);
+
     }
 
     @Test
     public void pathForDbScripts() throws Exception {
         String path = SDF_MysqlDatabase.getScriptPath("XX");
-        System.out.println(path);
+        //System.out.println(path);
         assertEquals(1, 1);
     }
 }

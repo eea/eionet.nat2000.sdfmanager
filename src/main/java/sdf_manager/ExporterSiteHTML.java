55184610aa6cf70d55c2fb6fddc13931b4400dea
kaido74 <kaido@tripledev.ee>
2014-05-23 11:54:37 +0300

    assign correct xsl to site html output Refs #19780

--- src/main/java/sdf_manager/ExporterSiteHTML.java
+++ src/main/java/sdf_manager/ExporterSiteHTML.java
@@ -21,15 +21,9 @@ import java.util.Iterator;
 import java.util.Set;
 import java.util.StringTokenizer;
 
-
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import pojos.*;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.transform.OutputKeys;
-
 import javax.xml.transform.Result;
 import javax.xml.transform.Source;
 import javax.xml.transform.Templates;
@@ -38,16 +32,35 @@ import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
-
-
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.w3c.dom.Document;
-
 import org.w3c.dom.Element;
-
 import org.xhtmlrenderer.pdf.ITextRenderer;
+
+import pojos.Biogeo;
+import pojos.Doc;
+import pojos.DocLink;
+import pojos.Habitat;
+import pojos.HabitatClass;
+import pojos.Impact;
+import pojos.Map;
+import pojos.Mgmt;
+import pojos.MgmtBody;
+import pojos.MgmtPlan;
+import pojos.NationalDtype;
+import pojos.OtherSpecies;
+import pojos.Ownership;
+import pojos.Region;
+import pojos.Resp;
+import pojos.Site;
+import pojos.SiteBiogeo;
+import pojos.SiteOwnership;
+import pojos.SiteRelation;
+import pojos.Species;
 import sdf_manager.util.SDF_Util;
 
 /**
@@ -186,6 +199,7 @@ public class ExporterSiteHTML implements Exporter {
      * @param fileName
      * @return
      */
+    @Override
     public boolean processDatabase(String fileName) {
         ExporterSiteHTML.log.error("Starting processDatabase. The file name is:::" + fileName);
         try {
@@ -768,7 +782,9 @@ public class ExporterSiteHTML implements Exporter {
             Result result = new StreamResult(file.toURI().getPath());
 
             TransformerFactory tFactory = TransformerFactory.newInstance();
-            Source xsl = new StreamSource("." + File.separator + "xsl" + File.separator + "SiteXSL.xsl");
+            String xslFileName = SDF_ManagerApp.isEmeraldMode() ? "EmeraldSiteXSL.xsl" : "SiteXSL.xsl";
+
+            Source xsl = new StreamSource("." + File.separator + "xsl" + File.separator + xslFileName);
             Templates template = tFactory.newTemplates(xsl);
             Transformer transformer = template.newTransformer();
             transformer.setOutputProperty(OutputKeys.INDENT, "yes");
@@ -987,6 +1003,7 @@ public class ExporterSiteHTML implements Exporter {
      * @param filename
      * @return
      */
+    @Override
     public ArrayList createXMLFromDataBase(String filename) {
         throw new UnsupportedOperationException("Not supported yet.");
     }

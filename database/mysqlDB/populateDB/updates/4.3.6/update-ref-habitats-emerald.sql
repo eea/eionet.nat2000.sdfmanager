4db1abce2c572fe4a3e6cb7c246aad672771ac7e
vskiadas90 <vskiadas90@gmail.com>
2020-01-15 15:39:05 +0200

    additional database changes through script - refs #112677

---
+++ database/mysqlDB/populateDB/updates/4.3.6/update-ref-habitats-emerald.sql
@@ -0,0 +1,1 @@
+UPDATE ref_habitats_emerald SET REF_HABITATS_DESC_EN = 'Deep-sea rock and artificial hard substrata (but excluding A6.12 Deep sea artificial hard substrata)' WHERE  REF_HABITATS_CODE = 'A6.1';
